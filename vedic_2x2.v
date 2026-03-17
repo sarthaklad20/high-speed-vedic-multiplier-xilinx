3902cf74802b4ba1eb62415ccda85cb9c515438f
Sarthak lad <sarthaklad20@gmail.com>
2026-03-17 23:43:31 +0530

    Clean up header comments in vedic_2x2.v
    Removed header comments and unnecessary lines.

--- vedic_2x2.v
+++ vedic_2x2.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.03.2026 22:57:21
-// Design Name: 
-// Module Name: vedic_2x2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module vedic_2x2(
 input [1:0] A,
 input [1:0] B,
